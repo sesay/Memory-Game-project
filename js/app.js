55cd0d5020f531a7c37a119b391afb06e0c7d4b9
sesay umar <ssesay@altayer.com>
2018-12-03 18:39:32 +0400

    feat: reset game board and reset counter to null

--- js/app.js
+++ js/app.js
@@ -7,6 +7,16 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -52,19 +62,6 @@ var initGame = function () {
 
 initGame();
 
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
 var cards = document.querySelectorAll('.card');
 var openCards = [];
 var counter = 0;
@@ -85,6 +82,7 @@ function activateCards() {
 }
 
 activateCards();
+clearGame();
 
 function compareCards(cardA, cardB) {
     if (cardA.dataset.match === cardB.dataset.match) {
@@ -105,6 +103,19 @@ function resetMatch(cardA, cardB) {
         cardA.classList.remove('open', 'show');
         cardB.classList.remove('open', 'show');
     }, 350);
+}
+
+function clearGame() {
+    var revertCards = document.querySelectorAll('.match');
+    var replay = document.querySelector('.restart');
+    revertCards.forEach(function (revertCard) {
+        revertCard.classList.remove('open','show','match');
+    }); 
+    replay.addEventListener('click', function () {
+        clearGame();
+        console.log('i was clicked..');
+    });
+    updateCount(0);
 }
 
 
